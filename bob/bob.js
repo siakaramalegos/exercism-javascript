83bebc41ab9f2600de72e5e5ed23c99fcbf207aa
Sia <siakaramalegos@gmail.com>
2015-02-05 19:39:07 -0600

    Complete Bob JavaScript exercism

--- bob/bob.js
+++ bob/bob.js
@@ -6,9 +6,18 @@
 var Bob = function() {};
 
 Bob.prototype.hey = function(input) {
-//
-// YOUR CODE GOES HERE
-//
+  if (input.trim() == '') {
+    return 'Fine. Be that way!';
+  }
+  else if (input == input.toUpperCase() && /[a-z]/i.test(input)){
+    return 'Whoa, chill out!';
+  }
+  else if (input.slice(-1) == '?') {
+    return 'Sure.';
+  }
+  else {
+    return 'Whatever.';
+  };
 };
 
 module.exports = Bob;
